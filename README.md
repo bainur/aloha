81f670d8fb7689317a4d9543951a532aa1352182
inoe <bainur_budi@yahoo.com>
2014-04-23 19:19:54 +0700

    Added parameters and updated readme

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # Aloha
 
-TODO: Write a gem description
+Aloha is a gem to access the web services
 
 ## Installation
 
 Add this line to your application's Gemfile:
 
-    gem 'aloha'
+```ruby
+gem 'simple_form'
+```
 
 And then execute:
 
-    $ bundle
+    $ bundle install
 
 Or install it yourself as:
 
@@ -18,7 +20,126 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+The first, you create a variable eg. `client` without parameter like this :
+```ruby
+client = Aloha.soap_setting
+```
+or use parameter :
+```ruby
+client = Aloha.soap_setting(system_id: 'system ID', company_id: 'company ID', user_id: 'user ID', password: 'password', wsdl_url: 'https://memberlinkWS.alohaenterprise.com/insightws/MemberLinkWS?wsdl')
+```
+
+Then, we can access for :
+
+Add member profile without parameter :
+```ruby
+Aloha.add_member_profile(client)
+```
+Add member profile with parameters :
+```ruby
+Aloha.add_member_profile(client, member_account_id: :integer, card_number: :string, first_name: :string, last_name: :string, company: :string, date_of_birth: {date: :string, locale: :string}, anniversary_date: {date: :string, locale: :string}, drivers_license: :string, address1: :string, address2: :string, city: :string, state_province: :string, country: :string, postal_code: :string, email_address: :string, phone_number: :string, other_phone_number: :string, profile_exists: :boolean)
+```
+
+Adjust credit without parameter
+```ruby
+Aloha.adjust_credit(client)
+```
+Adjust credit with parameters
+```ruby
+Aloha.adjust_credit(client, card_number: :string, bpid: :integer, adjustment_type: :string, bp_credit: :integer, reason: :string)
+```
+
+Get bonus plan history without parameter
+```ruby
+Aloha.get_bonus_plan_history(client)
+```
+Get bonus plan history without parameters
+```ruby
+Aloha.get_bonus_plan_history(client, card_number: :string,  number_of_assignments: :integer, number_of_days: :integer, start_date: :string,  end_date: :string)
+```
+
+Check email exists without parameter
+```ruby
+Aloha.email_exists(client)
+```
+Check email exists with parameter
+```ruby
+Aloha.email_exists(client, email_address: :string)
+```
+
+Get card number by email without parameter
+```ruby
+Aloha.get_card_number_by_email(client)
+```
+Get card number by email with parameter
+```ruby
+Aloha.get_card_number_by_email(client, email_address: :string)
+```
+
+
+Get card number by phone without parameter
+```ruby
+Aloha.get_card_number_by_phone(client)
+```
+Get card number by phone with parameter
+```ruby
+Aloha.get_card_number_by_phone(client, phone_number: :string)
+```
+
+Get card status without parameter
+```ruby
+Aloha.get_card_status(client)
+```
+Get card status with parameter
+```ruby
+Aloha.get_card_status(client, card_number: :string)
+```
+
+Get member profile without parameter
+```ruby
+Aloha.get_member_profile(client)
+```
+Get member profile with parameter
+```ruby
+Aloha.get_member_profile(client, card_number: :string)
+```
+
+Check phone number exists without parameter
+```ruby
+Aloha.phone_number_exists(client)
+```
+Check card number by phone with parameter
+```ruby
+Aloha.phone_number_exists(client, phone_number: :string)
+```
+
+Update member profile without parameter
+```ruby
+Aloha.update_member_profile(client)
+```
+Update member profile with parameter
+```ruby
+Aloha.update_member_profile(client, member_account_id: :integer, card_number: :string, first_name: :string, last_name: :string, company: :string, date_of_birth: {date: :string, locale: :string}, anniversary_date: {date: :string, locale: :string}, drivers_license: :string, address1: :string, address2: :string, city: :string, state_province: :string, country: :string, postal_code: :string, email_address: :string, phone_number: :string, other_phone_number: :string, profile_exists: :boolean)
+```
+
+
+Assign forgotten card without parameter
+```ruby
+Aloha.assign_forgotten_card(client)
+```
+Assign forgotten card with parameter
+```ruby
+Aloha.assign_forgotten_card(client, card_number: :string, claim_id: :string)
+```
+
+Get check detail without parameter
+```ruby
+Aloha.get_check_detail(client)
+```
+Get check detail with parameter
+```ruby
+Aloha.get_check_detail(client, claim_id: :string, store_id: :integer, date_of_business: :string)
+
 
 ## Contributing
 
--- lib/aloha.rb
+++ lib/aloha.rb
@@ -4,34 +4,43 @@ require 'savon'
 require 'httpclient'
 
 module Aloha
+  gem_root = Gem::Specification.find_by_name('aloha').gem_dir
+  @default_setting = YAML.load_file(gem_root + '/config/default_setting.yml')
 
-  system_id = 'rr39261'
-  company_id = 'RDS11'
-  user_id = 'Web.Service'
-  pwd = 'LoyaltyAPI1'
-  password='!069158823A'
-  default_request = {'company_id' => company_id, 'userID' => user_id, 'password' => pwd}
-  wsdl_url = 'https://memberlinkWS.alohaenterprise.com/insightws/MemberLinkWS?wsdl'
+  # Setting for soap client
+  def self.soap_setting(params = {})
+    setting = @default_setting['soap_setting']
 
-  @client = Savon.client do
-    wsdl wsdl_url
-    wsse_timestamp true
-    wsse_auth system_id, password
-    log true
-    log_level :debug
-    pretty_print_xml true
+    system_id = params[:system_id] || setting['system_id']
+    company_id = params[:company_id] || setting['company_id']
+    user_id = params[:user_id] || setting['user_id']
+    pwd = params[:pwd] || setting['pwd']
+    password = params[:password] || setting['password']
+    default_request = {'company_id' => company_id, 'userID' => user_id, 'password' => pwd}
+    wsdl_url = params[:wsdl_url] || setting['wsdl_url']
+
+    @client = Savon.client do
+      wsdl wsdl_url
+      wsse_timestamp true
+      wsse_auth system_id, password
+      log true
+      log_level :debug
+      pretty_print_xml true
+    end
   end
 
   #### addMemberProfile()
-  def self.add_member_profile(params = {})
-    @client.call(:add_member_profile) do
+  def self.add_member_profile(client, params = {})
+    profile = @default_setting['member_profile']
+
+    client.call(:add_member_profile) do
       message(add_member_profile_request: {
         profile: {
-          member_account_id: params[:member_account_id] || 1, # memberAccountID:int
-          card_number: params[:card_number] || '424242424242', # cardNumber:String
-          first_name: params[:first_name] || 'First Name',  # firstName:String
-          last_name: params[:last_name] || 'Last Name', # lastName:String
-          company: params[:company] || 'Company', # company:String
+          member_account_id: params[:member_account_id] || profile['member_account_id'], # memberAccountID:int
+          card_number: params[:card_number] || profile['card_number'], # cardNumber:String
+          first_name: params[:first_name] || profile['first_name'],  # firstName:String
+          last_name: params[:last_name] || profile['last_name'], # lastName:String
+          company: params[:company] || profile['company'], # company:String
           date_of_birth: {
             date: params[:date_of_birth].blank? ? Date.today.to_s : params[:date_of_birth][:date].to_s || Date.today.to_s, # VanityDate date:String
             locale: params[:date_of_birth].blank? ? 'en_US' : params[:date_of_birth][:locale].to_s || 'en_US' # VanityDate locale:String
@@ -40,152 +49,159 @@ module Aloha
             date: params[:anniversary_date].blank? ? Date.today.to_s : params[:anniversary_date][:date].to_s || Date.today.to_s,  # VanityDate date:String
             locale: params[:anniversary_date].blank? ? 'en_US' : params[:anniversary_date][:locale].to_s || 'en_US' # VanityDate locale:String
           },
-          drivers_license: params[:drivers_license] || '1234567890',
-          address1: params[:address1] || 'Address 1', # address1:String
-          address2: params[:address2] || 'Address 2', # address2:String
-          city: params[:city] || 'City', # city:String
-          state_province: params[:state_province] || 'State Province', # stateProvince:String
-          country: params[:country]|| 'Country', # country:String
-          postal_code: params[:postal_code] || '12345', # postalCode:String
-          email_address: params[:email_address] || 'test@example.com', # emailAddress:String
-          phone_number: params[:phone_number] || '+62891000000', # phoneNumber:String
-          other_phone_number: params[:other_phone_number] || '+6222900000', # otherPhoneNumber:String
+          drivers_license: params[:drivers_license] || profile['drivers_license'], # driversLicense:String
+          address1: params[:address1] || profile['address1'], # address1:String
+          address2: params[:address2] || profile['address2'], # address2:String
+          city: params[:city] || profile['city'], # city:String
+          state_province: params[:state_province] || profile['state_province'], # stateProvince:String
+          country: params[:country]|| profile['country'], # country:String
+          postal_code: params[:postal_code] || profile['postal_code'], # postalCode:String
+          email_address: params[:email_address] || profile['email_address'], # emailAddress:String
+          phone_number: params[:phone_number] || profile['phone_number'], # phoneNumber:String
+          other_phone_number: params[:other_phone_number] || profile['other_phone_number'], # otherPhoneNumber:String
           profile_exists: params[:profile_exists] # profileExists:Boolean
         }
       })
-    end
+    end if client
   end
 
   ### adjustCredit()
-  def self.adjust_credit(params = {})
-    @client.call(:adjust_credit) do
+  def self.adjust_credit(client, params = {})
+    adjust_credit = @default_setting['adjust_credit']
+
+    client.call(:adjust_credit) do
       message(adjust_credit_request: {
-        'cardNumber' => params[:card_number] || '4242424242424242', # cardNumber:String
-        'BPID' => params[:bpid] || 1, # BPID:Int
-        'adjustmentType' => params[:adjustment_type] || 1234500000001, # adjustmentType:String
-        'BPCredit' => params[:bp_credit] || 1, # BPCredit:Int
-        'reason' => params[:reason] || 'test reason' # reason:String
+        'cardNumber' => params[:card_number] || adjust_credit['card_number'], # cardNumber:String
+        'BPID' => params[:bpid] || adjust_credit['bpid'], # BPID:Int
+        'adjustmentType' => params[:adjustment_type] || adjust_credit['adjustment_type'], # adjustmentType:String
+        'BPCredit' => params[:bp_credit] || adjust_credit['bp_credit'], # BPCredit:Int
+        'reason' => params[:reason] || adjust_credit['reason'] # reason:String
       })
-    end
+    end if client
   end
 
   ### getBonusPlanHistory()
-  def self.get_bonus_plan_history(params = {})
-    @client.call(:get_bonus_plan_history) do
+  def self.get_bonus_plan_history(client, params = {})
+    plan_history = @default_setting['get_bonus_plan_history']
+
+    client.call(:get_bonus_plan_history) do
       message('GetBonusPlanHistoryRequest' => {
-        card_number: params[:card_number] || '4242424242424242', # cardNumber:String
-        number_of_assignments: params[:number_of_assignments] || 1, # numberOfAssignments:Int
-        number_of_days: params[:number_of_days] || 1, # numberDfDays:Int
+        card_number: params[:card_number] || plan_history['card_number'], # cardNumber:String
+        number_of_assignments: params[:number_of_assignments] || plan_history['number_of_assignments'], # numberOfAssignments:Int
+        number_of_days: params[:number_of_days] || plan_history[:number_of_days], # numberDfDays:Int
         start_date: params[:start_date] || Date.today.to_s, # startDate:String
         end_date: params[:end_date] || Date.today.to_s # endDate:String
       })
-    end
+    end if client
   end
 
   ### getBonusPlanStandings()
-  def self.get_bonus_plan_standings(params = {})
-    @client.call(:get_bonus_plan_standings) do
+  def self.get_bonus_plan_standings(client, params = {})
+    client.call(:get_bonus_plan_standings) do
       message(get_bonus_plan_standings_request: {
         card_number: params[:card_number] || '42424242424242', # cardNumber:String
         available_items_return: params[:available_items_return], #availableItemsReturn:Boolean
         purchased_items_return: params[:purchased_items_return] #purchasedItemsReturn:Boolean
       })
-    end
+    end if client
   end
 
   ### eMailExists()
-  def self.email_exists(params = {})
-    @client.call(:e_mail_exists) do
+  def self.email_exists(client, params = {})
+    client.call(:e_mail_exists) do
       message(e_mail_exists_request: {e_mail_address: params[:e_mail_address] || 'test@example.com'}) # eMailAddress:String
-    end
+    end if client
   end
 
   ### getCardNumberByEmail()
-  def self.get_card_number_by_email(params = {})
-    @client.call(:get_card_number_by_email) do
+  def self.get_card_number_by_email(client, params = {})
+    client.call(:get_card_number_by_email) do
       message('GetCardNumberByEmailRequest' => {email_address: params[:email_address] || 'test@example.com'}) # emailAddress:String
-    end
+    end if client
   end
 
   ### getCardNumberByPhone()
-  def self.get_card_number_by_phone(params = {})
+  def self.get_card_number_by_phone(client, params = {})
     @client.call(:get_card_number_by_phone) do
       message('GetCardNumberByPhoneRequest' => {phone_number: params[:phone_number] || '+6222900000'}) # phoneNumber:String
     end
   end
 
   ### getCardStatus()  
-  def self.get_card_status(params = {})
-    @client.call(:get_card_status) do
+  def self.get_card_status(client, params = {})
+    client.call(:get_card_status) do
       message(get_card_status_request: {card_number: params[:card_number] || '42424242424242'}) # cardNumber:String 14 digit
-    end
+    end if client
   end
 
   ### getMemberProfile()  
-  def self.get_member_profile(params = {})
-    @client.call(:get_member_profile) do
+  def self.get_member_profile(client, params = {})
+    client.call(:get_member_profile) do
       message(get_member_profile_request: {card_number: params[:card_number] || '42424242424242'}) # cardNumber:String 14 digit
-    end
+    end if client
   end
 
   ### phoneNumberExists() 
-  def self.phone_number_exists(params = {})
-    @client.call(:phone_number_exists) do
+  def self.phone_number_exists(client, params = {})
+    client.call(:phone_number_exists) do
       message('PhoneNumberExistsRequest' => {phone_number: params[:card_number] || '+62229000000'}) # phoneNumber:String
-    end
+    end if client
   end
 
   ### updateMemberProfile()
-  def self.update_member_profile(params = {})
-    @client.call(:update_member_profile) do
+  def self.update_member_profile(client, params = {})
+    profile = @default_setting['member_profile']
+
+    client.call(:update_member_profile) do
       message(update_member_profile_request: {
         profile: {
-          member_account_id: params[:member_account_id] || 1, # memberAccountID:int
-          card_number: params[:card_number] || '424242424242', # cardNumber:String
-          first_name: params[:first_name] || 'First Name',  # firstName:String
-          last_name: params[:last_name] || 'Last Name', # lastName:String
-          company: params[:company] || 'Company', # company:String
+          member_account_id: params[:member_account_id] || profile['member_account_id'], # memberAccountID:int
+          card_number: params[:card_number] || profile['card_number'], # cardNumber:String
+          first_name: params[:first_name] || profile['first_name'],  # firstName:String
+          last_name: params[:last_name] || profile['last_name'], # lastName:String
+          company: params[:company] || profile['company'], # company:String
           date_of_birth: {
-              date: params[:date_of_birth].blank? ? Date.today.to_s : params[:date_of_birth][:date].to_s || Date.today.to_s,
-              locale: params[:date_of_birth].blank? ? 'en_US' : params[:date_of_birth][:locale].to_s || 'en_US'
+            date: params[:date_of_birth].blank? ? Date.today.to_s : params[:date_of_birth][:date].to_s || Date.today.to_s, # VanityDate date:String
+            locale: params[:date_of_birth].blank? ? 'en_US' : params[:date_of_birth][:locale].to_s || 'en_US' # VanityDate locale:String
           },
           anniversary_date: {
-              date: params[:anniversary_date].blank? ? Date.today.to_s : params[:anniversary_date][:date].to_s || Date.today.to_s,
-              locale: params[:anniversary_date].blank? ? 'en_US' : params[:anniversary_date][:locale].to_s || 'en_US'
+            date: params[:anniversary_date].blank? ? Date.today.to_s : params[:anniversary_date][:date].to_s || Date.today.to_s,  # VanityDate date:String
+            locale: params[:anniversary_date].blank? ? 'en_US' : params[:anniversary_date][:locale].to_s || 'en_US' # VanityDate locale:String
           },
-          drivers_license: params[:drivers_license] || '1234567890',
-          address1: params[:address1] || 'Address 1', # address1:String
-          address2: params[:address2] || 'Address 2', # address2:String
-          city: params[:city] || 'City', # city:String
-          state_province: params[:state_province] || 'State Province', # stateProvince:String
-          country: params[:country]|| 'Country', # country:String
-          postal_code: params[:postal_code] || '12345', # postalCode:String
-          email_address: params[:email_address] || 'test@example.com', # emailAddress:String
-          phone_number: params[:phone_number] || '+62891000000', # phoneNumber:String
-          other_phone_number: params[:other_phone_number] || '+6222900000' # otherPhoneNumber:String
+          drivers_license: params[:drivers_license] || profile['drivers_license'],
+          address1: params[:address1] || profile['address1'], # address1:String
+          address2: params[:address2] || profile['address2'], # address2:String
+          city: params[:city] || profile['city'], # city:String
+          state_province: params[:state_province] || profile['state_province'], # stateProvince:String
+          country: params[:country]|| profile['country'], # country:String
+          postal_code: params[:postal_code] || profile['postal_code'], # postalCode:String
+          email_address: params[:email_address] || profile['email_address'], # emailAddress:String
+          phone_number: params[:phone_number] || profile['phone_number'], # phoneNumber:String
+          other_phone_number: params[:other_phone_number] || profile['other_phone_number'], # otherPhoneNumber:String
+          profile_exists: params[:profile_exists] # profileExists:Boolean
         }
       })
-    end
+    end if client
   end
 
   ### assignForgottenCard()
-  def self.assign_forgotten_card(params = {})
-    @client.call(:assign_forgotten_card) do
+  def self.assign_forgotten_card(client, params = {})
+    client.call(:assign_forgotten_card) do
       message(assign_forgotten_card_request: {
           card_number: params[:card_number] || '4242424242424242', # cardNumber:String
           claim_ID: params[:claim_id] || '12345678910' # claimID:String
       })
-    end
+    end if client
   end
 
   ### getCheckDetail()
-  def self.get_check_detail(params = {})
-    @client.call(:get_check_detail) do
+  def self.get_check_detail(client, params = {})
+    client.call(:get_check_detail) do
       message('GetCheckDetailRequest' => {
         claim_ID: params[:claim_id] || 'testexample123123', # claimID:String
         store_ID: params[:store_id] || 1122334455, # store_ID:Int
         date_of_business: params[:date_of_business] || Date.today.to_s # dateOfBusiness:String
       })
-    end
+    end if client
   end
 end
