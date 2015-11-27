bbddaef419a4c4d82cf439c2c54668ca0af52e4b
syafik <syafiklijtk04@gmail.com>
2015-11-27 13:35:57 +0700

    added get card balance and get sv history

--- lib/aloha.rb
+++ lib/aloha.rb
@@ -25,9 +25,9 @@ module Aloha
         wsdl wsdl_url
         wsse_timestamp true
         wsse_auth system_id, system_password
-        log true
-        log_level :debug
-        pretty_print_xml true
+        #log false
+        #log_level :error
+        #pretty_print_xml true
         ssl_version ssl_version
       end if  system_id && system_password
     end
