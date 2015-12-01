fd366aedd006b0dba1b726bc65ee360fee58a506
syafik <syafiklijtk04@gmail.com>
2015-12-01 10:21:09 +0700

    show log request

--- lib/aloha.rb
+++ lib/aloha.rb
@@ -26,7 +26,7 @@ module Aloha
         wsse_timestamp true
         wsse_auth system_id, system_password
         log true
-        log_level :info
+        log_level :debug
         pretty_print_xml true
         ssl_version ssl_version
       end if  system_id && system_password
